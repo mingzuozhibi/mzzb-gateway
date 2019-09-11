f589f3b5060541ab4d6bc437446d11da7428c570
fuhaiwei <fuhaiwei@126.com>
2019-09-11 21:45:47 +0800

    feat: Add ModuleConnectListener

---
+++ src/main/java/mingzuozhibi/gateway/Message.java
@@ -0,0 +1,19 @@
+package mingzuozhibi.gateway;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Message {
+
+    private String type;
+    private String text;
+    private Instant createOn;
+    private Instant acceptOn;
+
+}
--- src/main/java/mingzuozhibi/gateway/MzzbGatewayApplication.java
+++ src/main/java/mingzuozhibi/gateway/MzzbGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MzzbGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MzzbGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MzzbGatewayApplication.class, args);
+    }
 
 }
