d0402f1fff469089a02f8b48aab263fce0e492c6
fuhaiwei <fuhaiwei@126.com>
2019-09-11 20:15:57 +0800

    feat: Add From SpringBoot

---
+++ src/main/java/mingzuozhibi/gateway/MzzbGatewayApplication.java
@@ -0,0 +1,13 @@
+package mingzuozhibi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzzbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzzbGatewayApplication.class, args);
+	}
+
+}
