0bb06ea252621b32e97f5cece540a073b2c1aee9
fuhaiwei <fuhaiwei@126.com>
2019-09-14 00:03:59 +0800

    feat: Use Common 0.2.4

--- src/main/java/mingzuozhibi/gateway/connect/ConnectListener.java
+++ src/main/java/mingzuozhibi/gateway/connect/ConnectListener.java
@@ -3,6 +3,7 @@ package mingzuozhibi.gateway.connect;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+import mingzuozhibi.common.jms.JmsMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component;
 public class ConnectListener {
 
     @Autowired
+    private JmsMessage jmsMessage;
+    @Autowired
     private ConnectHelper connectHelper;
 
     @JmsListener(destination = "module.connect")
@@ -20,7 +23,7 @@ public class ConnectListener {
         String name = root.get("name").getAsString();
         String addr = root.get("addr").getAsString();
         connectHelper.setModuleAddr(name, addr);
-        log.info("JMS <- module.connect [name={}, addr={}]", name, addr);
+        jmsMessage.notify(String.format("JMS <- module.connect [name=%s, addr=%s]", name, addr));
     }
 
 }
--- src/main/java/mingzuozhibi/gateway/modules/discshelfs/DiscShelfsListener.java
+++ src/main/java/mingzuozhibi/gateway/modules/discshelfs/DiscShelfsListener.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import mingzuozhibi.common.BaseController;
+import mingzuozhibi.common.jms.JmsMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import javax.annotation.Resource;
 @Slf4j
 @RestController
 public class DiscShelfsListener extends BaseController {
+
+    @Autowired
+    private JmsMessage jmsMessage;
 
     @Resource(name = "redisTemplate")
     private SetOperations<String, String> setOps;
@@ -24,6 +29,7 @@ public class DiscShelfsListener extends BaseController {
         String asin = root.get("asin").getAsString();
         setOps.add("disc.track", asin);
         log.info("JMS <- disc.track [name={}, asin={}]", name, asin);
+        jmsMessage.notify(String.format("JMS <- disc.track [name=%s, asin=%s]", name, asin));
     }
 
 }
